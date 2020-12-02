be6ad8d3d2575370aef42b226cfc614176267a20
Bartu Bozkurt <45657409+bartubozkurt@users.noreply.github.com>
2020-12-02 20:11:14 +0300

    Create matrixMultiply.py

---
+++ matrixMultiply.py
@@ -0,0 +1,24 @@
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+    # 3*3 matrix
+    
+Y = [[5,8,1,2],
+    [6,7,3,10],
+    [4,5,9,1]]
+    # 3*4 matrix
+    
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+         # 3*4 matrix
+         
+# iterate through rows of X        
+for i in range(len(X)):
+     # iterate through columns of Y
+    for j in range(len(Y[0])):
+        # iterate through rows of Y
+        for  k in range(len(Y)):
+            result[i][j] += X[i][k] * Y[k][j]
+for r in result:
+    print(r)
